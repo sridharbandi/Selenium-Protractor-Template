5c01280eb32dc0ff0e171a1d466cb2e822ff0d02
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2020-04-19 22:10:23 +0200

    Converted to ES6

--- specs/spec.js
+++ specs/spec.js
@@ -1,14 +1,13 @@
-browser.ignoreSynchronization = true;
-var GoogleSearchPage = require('../pageobjects/googlesearchpage');
-var SearchResultsPage = require('../pageobjects/searchresultspage');
+import GoogleSearchPage from '../pageobjects/googlesearchpage';
+import SearchResultsPage from '../pageobjects/searchresultspage';
 
-describe('Verify Google Title', function() {
-    it('should have a title', async function() {
+describe('Verify Google Title', function () {
+    it('should have a title', async function () {
         await GoogleSearchPage.open();
         expect(await GoogleSearchPage.title()).toEqual('Google');
     });
 
-    it('Search for Selenium', async function() {
+    it('Search for Selenium', async function () {
         await GoogleSearchPage.searchFor('Selenium');
         expect(await SearchResultsPage.isSeleniumPresent()).toBeTruthy();
     });
